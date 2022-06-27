32eee82d7d89c5177817bcd1094fdc5deb1e1c32
Axely7 <axely7@hotmail.com>
2022-06-27 15:19:44 -0500

    Updating entry

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,5 +1,4 @@
 import { FC, useEffect, useReducer } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import { entriesApi } from '../../apis';
 import { Entry } from '../../interfaces';
 import { EntriesContext, entriesReducer  } from './';
@@ -20,16 +19,11 @@ export const EntriesProvider:FC<Props> = ({ children }) => {
 
     const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE);
 
-    const addNewEntry = ( description: string ) => {
+    const addNewEntry = async ( description: string ) => {
 
-        const newEntry: Entry = {
-            _id: uuidv4(),
-            description,
-            createdAt: Date.now(),
-            status: 'pending'
-        }
+        const { data } = await entriesApi.post<Entry>('/entries', { description });
 
-        dispatch({ type: '[Entry] Add-Entry', payload: newEntry })
+        dispatch({ type: '[Entry] Add-Entry', payload: data })
     }
 
     const updateEntry = ( entry: Entry ) => {
